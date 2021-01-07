67d014e7fd12ab030665e40841d34a8c6dc80c8a
sseans <41116820+sseans@users.noreply.github.com>
2021-01-07 15:58:38 -0700

    further testing

--- Testing/test.js
+++ Testing/test.js
@@ -1,10 +1,37 @@
 const button = document.querySelector('.button');
+const contentBox = document.querySelector('.contentContainer');
 
 button.addEventListener('click', modeActivate)
 
 function modeActivate() {
-    document.querySelector('.contentContainer').id = 'animation';
+    //Move Div Up
+    document.querySelector('.textContainer').id = 'animation';
     console.log('function-accessed');
+    // Create Div
+    setTimeout(createDiv, 800)
+    // Remove Div
+    setTimeout(function() {
+        removeDiv('textContainer')
+    }, 500)
     
 }
 
+function removeDiv(className) {
+    let elements = document.getElementsByClassName(className);
+    if (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function createDiv() {
+    // Create Div
+    let element = document.createElement('div')
+    contentBox.appendChild(element)
+    element.className = 'modeActivated'
+    // Create Sub Divs
+    for (let i = 0; i < 12; i++) {
+        let moviePosters = document.createElement('div')
+        element.appendChild(moviePosters)
+        moviePosters.className = 'moviePosters'
+    }
+}
