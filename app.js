691998189048f1c8196e725f5897d73c0d4be5e9
sseans <41116820+sseans@users.noreply.github.com>
2021-01-08 18:24:04 -0700

    ui color change for movie mode

--- app.js
+++ app.js
@@ -9,16 +9,19 @@ movieButton.addEventListener('click', movieMode)
 
 // Functions
 function movieMode() {
+    // Play animation / Remove animated divs
     document.querySelector('.content').id = 'animationUp';
     setTimeout(function() {removeDiv('content')}, 175);
-    setTimeout(colorChange, 40)
     setTimeout(function() {removeDiv('raspberryFooter')}, 175);
+    // Change BG color
+    setTimeout(colorChange, 40)
+    // Create movie mode divs / Play animation
     setTimeout(createMoviePosters, 100)
     setTimeout(moviePosterAnimationPlay, 125)
-    
 }
 
 function removeDiv(className) {
+    // Goes through array to remove the div and every child element
     let elements = document.getElementsByClassName(className);
     if (elements.length > 0) {
         elements[0].parentNode.removeChild(elements[0]);
@@ -26,10 +29,9 @@ function removeDiv(className) {
 }
 
 function colorChange() {
-    // Color Change for after background color changed
+    // Color Change  various toolbar elements after background color changed
     document.querySelector('.defaultBody').className = 'postAnimationBody';
     document.querySelector('.toolbarTitle').className = 'toolbarTitleAfter';
-
     // Changing classnames for multiple items in the Toolbar, 3 for the LI elements, 2 for dividers
     for(let i = 0; i < 3; i++) {
         document.querySelector('.toolbarButtons').className = 'toolbarButtonsAfter';
@@ -37,14 +39,15 @@ function colorChange() {
             document.querySelector('.divider').className = 'dividerAfter';
         }
     }
-    
 }
 
 function createMoviePosters() {
+    // Create & append
     const movieMode = document.createElement('div')
     document.body.appendChild(movieMode)
     movieMode.className = 'movieModeContainer'
-    for (let i = 0; i < 12; i++) {
+    // Create individual posterdivs
+    for (let i = 0; i < 16; i++) {
         const moviePosters = document.createElement('div')
         movieMode.appendChild(moviePosters)
         moviePosters.className = 'moviePosters'
