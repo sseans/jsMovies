5f1aca22378b83479cd5d9fc2ad1b3a918b31a72
sseans <41116820+sseans@users.noreply.github.com>
2021-01-08 18:09:47 -0700

    basic view movies functionality implemented, blank posters, no api yet

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ movieButton.addEventListener('click', movieMode)
 // Functions
 function movieMode() {
     document.querySelector('.content').id = 'animationUp';
-    setTimeout(function() {removeDiv('content')}, 300);
-    setTimeout(bodyChange, 100)
+    setTimeout(function() {removeDiv('content')}, 175);
+    setTimeout(colorChange, 40)
+    setTimeout(function() {removeDiv('raspberryFooter')}, 175);
+    setTimeout(createMoviePosters, 100)
+    setTimeout(moviePosterAnimationPlay, 125)
+    
 }
 
 function removeDiv(className) {
@@ -21,11 +25,35 @@ function removeDiv(className) {
     }
 }
 
-function bodyChange() {
+function colorChange() {
+    // Color Change for after background color changed
     document.querySelector('.defaultBody').className = 'postAnimationBody';
+    document.querySelector('.toolbarTitle').className = 'toolbarTitleAfter';
+
+    // Changing classnames for multiple items in the Toolbar, 3 for the LI elements, 2 for dividers
+    for(let i = 0; i < 3; i++) {
+        document.querySelector('.toolbarButtons').className = 'toolbarButtonsAfter';
+        if (i < 2) {
+            document.querySelector('.divider').className = 'dividerAfter';
+        }
+    }
+    
 }
 
+function createMoviePosters() {
+    const movieMode = document.createElement('div')
+    document.body.appendChild(movieMode)
+    movieMode.className = 'movieModeContainer'
+    for (let i = 0; i < 12; i++) {
+        const moviePosters = document.createElement('div')
+        movieMode.appendChild(moviePosters)
+        moviePosters.className = 'moviePosters'
+    }
+}
 
+function moviePosterAnimationPlay() {
+    document.querySelector('.movieModeContainer').id = 'posterAnimationUp'
+}
 
 
 
