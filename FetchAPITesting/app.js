f33b6d285419742836aac0a3e0736bcd2d16f4a4
sseans <41116820+sseans@users.noreply.github.com>
2021-01-11 12:06:17 -0700

    implemented movie mode fetch functionality

--- FetchAPITesting/app.js
+++ FetchAPITesting/app.js
@@ -1,21 +1,79 @@
 // Variables
 const defaultURL = 'https://api.themoviedb.org/3/movie/550?api_key=672afe6c70446d4ff7c242f8bb0a3609'
 const API_KEY = '672afe6c70446d4ff7c242f8bb0a3609'
+const searchURL = 'https://api.themoviedb.org/3/search/movie?api_key=672afe6c70446d4ff7c242f8bb0a3609&query='
+const imageURL = 'https://image.tmdb.org/t/p/w185/'
+const trendingURL = 'https://api.themoviedb.org/3/trending/movie/week?api_key=672afe6c70446d4ff7c242f8bb0a3609'
 
 // Selectors
 const inputElement = document.querySelector('.searchBox')
 const inputButton = document.querySelector('.searchButton')
+const movieSearchResults = document.querySelector('.movieSearchResults')
+const trendingButton = document.querySelector('.trendingButton')
 
 // Event Listeners
-inputButton.addEventListener('click', active)
+inputButton.addEventListener('click', fetchMovies)
+trendingButton.addEventListener('click', fetchTrending)
 
 // Functions
-function active() {
-    let inputValue = inputElement.value
-    console.log(inputValue)
-    
+function fetchTrending() {
+    fetch(trendingURL)
+        .then((res) => res.json())
+        .then((data) => {
+            // data.results [] data is returned as an Array
+            const movies = data.results;
+            // call function to create div/posters and append to DOM
+            const movieBlock = createMovieContainer(movies)
+            movieSearchResults.appendChild(movieBlock)
+            // Log json data
+            console.log('Data: ', data);
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+        });
 }
 
-function searchMovies() {
 
+function fetchMovies() {
+    // Add input from text field to fetch URL
+    let inputValue = inputElement.value
+    let valueAddedUrl = searchURL + inputValue
+
+    fetch(valueAddedUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            // data.results [] data is returned as an Array
+            const movies = data.results;
+            // call function to create div/posters and append to DOM
+            const movieBlock = createMovieContainer(movies)
+            movieSearchResults.appendChild(movieBlock)
+            // Log json data
+            console.log('Data: ', data);
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+        });
+}
+
+function createMovieContainer(movies) {
+    // Create Div & change class
+    const movieElement = document.createElement('div')
+    movieElement.className = 'movie'
+
+    // Create HTML Template with poster images
+    const movieTemplate = `
+        ${movieImages(movies)}
+    ` 
+
+    // Add template to div HTML
+    movieElement.innerHTML = movieTemplate;
+    return movieElement;
+}
+
+function movieImages(movies) {
+    return movies.map((movie) => {
+        return `
+            <img src=${imageURL + movie.poster_path} data-movie-id=${movie.id}/>
+        `;
+    })
 }
--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 // Variables
+const defaultURL = 'https://api.themoviedb.org/3/movie/550?api_key=672afe6c70446d4ff7c242f8bb0a3609'
+const API_KEY = '672afe6c70446d4ff7c242f8bb0a3609'
+const searchURL = 'https://api.themoviedb.org/3/search/movie?api_key=672afe6c70446d4ff7c242f8bb0a3609&query='
+const imageURL = 'https://image.tmdb.org/t/p/w185/'
+const trendingURL = 'https://api.themoviedb.org/3/trending/movie/week?api_key=672afe6c70446d4ff7c242f8bb0a3609'
 
 // Selectors
 const movieButton = document.getElementById('movieModeButton')
 const content = document.querySelector('.content')
+const inputElement = document.querySelector('.searchBar')
+const searchForm = document.querySelector('.inputContainer')
 
 // Event Listeners
 movieButton.addEventListener('click', movieMode)
+searchForm.addEventListener('submit', movieModeSearch)
 
-// Functions
+
+// Key Functions
+function movieModeSearch(Event) {
+    event.preventDefault();
+    // Play animation / Remove animated divs
+    document.querySelector('.content').id = 'animationUp';
+    setTimeout(function() {removeDiv('content')}, 175);
+    setTimeout(function() {removeDiv('raspberryFooter')}, 175);
+    // Change BG color
+    setTimeout(colorChange, 40)
+    // Create movie mode divs
+    setTimeout(fetchMovieSearch, 100)
+}
+
 function movieMode() {
     // Play animation / Remove animated divs
     document.querySelector('.content').id = 'animationUp';
@@ -15,11 +36,11 @@ function movieMode() {
     setTimeout(function() {removeDiv('raspberryFooter')}, 175);
     // Change BG color
     setTimeout(colorChange, 40)
-    // Create movie mode divs / Play animation
-    setTimeout(createMoviePosters, 100)
-    setTimeout(moviePosterAnimationPlay, 125)
+    // Create movie mode divs
+    setTimeout(fetchTrending, 100)
 }
 
+// Functions
 function removeDiv(className) {
     // Goes through array to remove the div and every child element
     let elements = document.getElementsByClassName(className);
@@ -41,24 +62,80 @@ function colorChange() {
     }
 }
 
-function createMoviePosters() {
-    // Create & append
-    const movieMode = document.createElement('div')
-    document.body.appendChild(movieMode)
-    movieMode.className = 'movieModeContainer'
-    // Create individual posterdivs
-    for (let i = 0; i < 16; i++) {
-        const moviePosters = document.createElement('div')
-        movieMode.appendChild(moviePosters)
-        moviePosters.className = 'moviePosters'
-    }
-}
-
 function moviePosterAnimationPlay() {
+    console.log('animationfunction activated');
+    
     document.querySelector('.movieModeContainer').id = 'posterAnimationUp'
 }
 
+// Fetch Functions
+function fetchTrending() {
+    fetch(trendingURL)
+        .then((res) => res.json())
+        .then((data) => {
+            // data.results [] data is returned as an Array
+            const movies = data.results;
+            // call function to create div/posters and append to DOM
+            const movieBlock = createMovieContainer(movies)
+            document.body.appendChild(movieBlock)
+            // Log json data
+            console.log('Data: ', data);
+            // Play Animation
+            moviePosterAnimationPlay()
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+        });
+}
 
+function fetchMovieSearch() {
+    // Add input from text field to fetch URL
+    let inputValue = inputElement.value
+    let valueAddedUrl = searchURL + inputValue    
+
+    fetch(valueAddedUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            // data.results [] data is returned as an Array
+            const movies = data.results;
+            // call function to create div/posters and append to DOM
+            const movieBlock = createMovieContainer(movies)
+            document.body.appendChild(movieBlock)
+            // Log json data
+            console.log('Data: ', data);
+            // Play Animation
+            moviePosterAnimationPlay()
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+        });
+}
+
+function createMovieContainer(movies) {
+    // Create Div & change class
+    const movieElement = document.createElement('div')
+    movieElement.className = 'movieModeContainer'
+
+    // Create HTML Template with poster images
+    let movieTemplate = `${movieImages(movies)}`;
+
+    // Add template to div HTML
+    movieElement.innerHTML = movieTemplate
+    
+    return movieElement;
+}
+
+function movieImages(movies) {
+    return movies.map((movie) => {
+        if (movie.poster_path == null) {
+            return
+        } else {
+            return `<div class='moviePosters'>
+                        <img src=${imageURL + movie.poster_path} data-movie-id=${movie.id}/>
+                    </div>`;
+        }
+    }).join('')
+}
 
 
 
