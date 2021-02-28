aca49f827e83344f323fe46a168faed2458785fe
sseans <41116820+sseans@users.noreply.github.com>
2021-02-28 12:59:20 -0700

    styling changes, changed method of displaying slider titles, added more code comments

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // Variables
     let movieObjectArray = []
     let sliderObjectArray = []
-    let preloadSlide = undefined
-    let currentActiveSlide = undefined
-    let unloadSlide = undefined
     const defaultURL = 'https://api.themoviedb.org/3/movie/550?api_key=672afe6c70446d4ff7c242f8bb0a3609'
     const API_KEY = '672afe6c70446d4ff7c242f8bb0a3609'
     const searchURL = 'https://api.themoviedb.org/3/search/movie?api_key=672afe6c70446d4ff7c242f8bb0a3609&query='
@@ -94,226 +91,66 @@
     }
 
     function sliderActive(appendDestination, imageURLSize) {
+        // Builds an Image with each Object in the Slider Array => Append to inner__slider
         sliderObjectArray.forEach(element => {
             let boundCreateSliderImages = element.createMovieSlide.bind(element)
             let slide = boundCreateSliderImages(imageURLSize)
             appendDestination.appendChild(slide)
         })
-        // Event Listener to identify direction
+
+        // Builds the first movie slider title to display on page load & keeps track of which title to display
+        let titleNumber = 0
+        sliderTitle(appendDestination, titleNumber)
+        
+        // leftAmount keeps track of the css styling property left: which moves the inner slider
         let leftAmount = 0
+        // Calculates the length of all the images build by the array, so you cant move past the images in the slider
         let maxAmount = (sliderObjectArray.length * 1280 - 1280) * -1
+
+        // Event Listener & if else statement to identify which button is clicked
         appendDestination.parentNode.addEventListener('click', (e) => {
             let clickedItem = e.target;
-            console.log(e.target);
-            console.log(maxAmount);
-            
-            
-
+        
             if (clickedItem.classList[1] == 'fa-chevron-right' || clickedItem.classList[0] == 'arrowright') {
                 if (leftAmount <= maxAmount) {
-                    console.log(leftAmount);
-
                     return
                 } else {
+                    titleNumber++
                     leftAmount -= 1280
-
-                    console.log(leftAmount);
-                    
                     appendDestination.style.left = `${leftAmount}px`;
+                    sliderTitleChange(appendDestination, titleNumber, 'right')
                 }
 
             } else if (clickedItem.classList[1] == 'fa-chevron-left' || clickedItem.classList[0] == 'arrowleft') {
                 if (leftAmount >= 0) {
-                    console.log(leftAmount);
-                    
                     return
                 } else {
+                    titleNumber--
                     leftAmount += 1280
-
-                    console.log(leftAmount);
-                    
                     appendDestination.style.left = `${leftAmount}px`;
+                    sliderTitleChange(appendDestination, titleNumber, 'left')
                 }
             }
         })
     }
 
-    // function sliderActive(appendDestination, imageURLSize) {
-    //     let slideNumber = 0
-    //     let direction = undefined
-    //     let activationCounter = undefined
+    // builds movie titles from slider object array => appends to slider div
+    function sliderTitle(appendDestination, titleNumber) {
+        let boundCreateInitialTitle = sliderObjectArray[titleNumber].buildSliderTitle.bind(sliderObjectArray[titleNumber])
+        sliderTitlePeice = boundCreateInitialTitle()
+        appendDestination.parentNode.appendChild(sliderTitlePeice)
+    }
 
-    //     // Set initial Slider img + state class
-    //     let boundCreateFirstSlideFunction = sliderObjectArray[slideNumber].createMovieSlide.bind(sliderObjectArray[slideNumber])
-    //     let initalSlide = boundCreateFirstSlideFunction(imageURLSize)
-    //     initalSlide.classList.toggle('active')
-    //     appendDestination.appendChild(initalSlide)
-        
-    //     currentActiveSlide = initalSlide;
+    // waits till half way through the slider animation (200ms) => deletes title => creates new title
+    function sliderTitleChange(appendDestination, titleNumber, direction) {
+        setTimeout(() => {
+            removeDiv('.sliderTitle')
+        }, 195);
 
-    //     slideNumber++
-
-    //     let boundCreateSecondSlideFunction = sliderObjectArray[slideNumber].createMovieSlide.bind(sliderObjectArray[slideNumber])
-    //     let secondSlide = boundCreateSecondSlideFunction(imageURLSize)
-    //     secondSlide.classList.toggle('preload')
-    //     appendDestination.appendChild(secondSlide)
-
-    //     preloadSlide = secondSlide;
-
-    //     // let sliderNumberForInitialUnloadSlide = (sliderObjectArray.length) - 1
-
-    //     // let boundCreateInitialUnloadSlideFunction = sliderObjectArray[sliderNumberForInitialUnloadSlide].createMovieSlide.bind(sliderObjectArray[sliderNumberForInitialUnloadSlide])
-    //     // let initialUnloadSlide = boundCreateInitialUnloadSlideFunction(imageURLSize)
-    //     // initialUnloadSlide.classList.toggle('unload')
-    //     // appendDestination.prepend(initialUnloadSlide)
-
-    //     // unloadSlide = initialUnloadSlide
-
-    //     // Event Listener to identify direction
-    //     appendDestination.addEventListener('click', (e) => {
-    //         let clickedItem = e.target;
-    //         if (clickedItem.classList[1] == 'fa-chevron-right') {
-
-    //             if (slideNumber == (sliderObjectArray.length)) {
-    //                 return
-    //             } else if (slideNumber < (sliderObjectArray.length)) {
-    //                 slideNumber++
-    //                 direction = 'right'                    
-    //                 moveSlider(direction, slideNumber, appendDestination, imageURLSize)
-    //                 activationCounter++
-    //             } else {
-    //                 return
-    //             }
-
-                
-    //         } else if (clickedItem.classList[1] == 'fa-chevron-left') {
-
-    //             if (activationCounter == 0) {
-    //                 if (slideNumber < 0) {
-    //                     return
-    //                 } else {
-    //                     slideNumber--
-    //                     direction = 'left'
-    //                     moveSlider(direction, slideNumber, appendDestination, imageURLSize)
-    //                 }
-    //             } else {
-    //                 if (slideNumber < -2) {
-    //                     return
-    //                 } else {
-    //                     slideNumber--
-    //                     direction = 'left'
-    //                     moveSlider(direction, slideNumber, appendDestination, imageURLSize)
-    //                 }
-    //             }
-
-                
-
-    //         }
-    //     })
-        
-    // }
-
-    // function moveSlider(direction, slideNumber, appendDestination, imageURLSize) {
-    //     if (direction == 'right') {
-    //         currentActiveSlide.classList.toggle('unload')
-    //         currentActiveSlide.classList.toggle('active')
-    //         preloadSlide.classList.toggle('active')
-    //         preloadSlide.classList.remove('preload')
-            
-
-    //         console.log(preloadSlide);
-            
-    //         console.log(currentActiveSlide);
-            
-    //         console.log(unloadSlide);
-
-    //         let removeSlide = unloadSlide
-            
-    //         if (slideNumber > 7) {
-    //             setTimeout(() => {
-    //                 removeSlide.remove()
-    //             }, 400);
-    //             console.log(`To right slideNumber: ${slideNumber}`);
-    //         } else {
-    //             setTimeout(() => {
-    //                 unloadSlide = currentActiveSlide;
-    //                 currentActiveSlide = preloadSlide
-        
-    
-    //                 console.log(`To right slideNumber: ${slideNumber}`);
-                    
-        
-    //                 let boundCreateSlideFunction = sliderObjectArray[slideNumber].createMovieSlide.bind(sliderObjectArray[slideNumber])
-    //                 let newPreloadSlide = boundCreateSlideFunction(imageURLSize)
-    //                 newPreloadSlide.classList.toggle('preload')
-    //                 appendDestination.appendChild(newPreloadSlide)
-        
-    //                 preloadSlide = newPreloadSlide
-    //             }, 5);
-        
-    //         }
-
-    //         console.log(preloadSlide);
-            
-    //         console.log(currentActiveSlide);
-            
-    //         console.log(unloadSlide);
-            
-    //         setTimeout(() => {
-    //             removeSlide.remove()
-    //         }, 400);
-
-    //     } else {
-    //         currentActiveSlide.classList.toggle('active')
-    //         currentActiveSlide.classList.toggle('unload')
-    //         console.log(preloadSlide);
-            
-    //         console.log(currentActiveSlide);
-            
-    //         console.log(unloadSlide);
-            
-
-    //         let removeSlide = preloadSlide
-
-    //         setTimeout(() => {
-    //             preloadSlide = currentActiveSlide;
-    //             currentActiveSlide = unloadSlide    
-                
-    //             let slideNumberForUnloadSlide = slideNumber -2
-    //             if (slideNumberForUnloadSlide < 0) {
-    //                 return
-    //             } else {
-    //                 let boundCreateSlideFunction = sliderObjectArray[slideNumberForUnloadSlide].createMovieSlide.bind(sliderObjectArray[slideNumberForUnloadSlide])
-    //                 let newUnloadSlide = boundCreateSlideFunction(imageURLSize)
-    //                 newUnloadSlide.classList.toggle('Unload')
-        
-    //                 unloadSlide = newUnloadSlide
-    //                 appendDestination.prepend(unloadSlide)
-
-    //             }
-    //             console.log(`To left modified slideNumber: ${slideNumberForUnloadSlide}`);
-    //             console.log(`To left slideNumber: ${slideNumber}`);
-                
-                
-                
-    //         }, 5);
-
-    //         console.log(preloadSlide);
-            
-    //         console.log(currentActiveSlide);
-            
-    //         console.log(unloadSlide);
-
-    //         // setTimeout(() => {
-    //         //     removeSlide.remove()
-    //         // }, 400);
-    
-            
-    //     }
-
-        
-        
-    // }
+        setTimeout(() => {
+            sliderTitle(appendDestination, titleNumber)            
+        }, 200);
+    }
 
 // Fetch Functions
     function fetchTrending(imageURLSize) {
@@ -367,6 +204,7 @@
     }
 
     function buildSliderObjectsFromMovieObjects() {
+        // filters original movie object array, finding only the ones with > 500 ratings => creates sliderObjectArray
         sliderObjectArray = movieObjectArray.filter((x) => {
             if (x.numberOfRatings > 500) {
                 return true
@@ -441,50 +279,24 @@ class movie {
     }
 
     createMovieBlock(appendDestination, imageURLSize) {        
-        let movieTemplate = undefined
-        let movieElement = undefined
+        // Create Div & change class
+        const movieElement = document.createElement('div')
+        movieElement.className = 'moviePosters'
 
-        if (imageURLSize == 'https://image.tmdb.org/t/p/w1280/') {
-            // Create Div & change class
-            movieElement = document.createElement('div')
-            movieElement.className = 'movieBackdrops'
+        // create Event Listener for clicking each poster
+        movieElement.addEventListener('click', () => this.toggleMovieView(movieElement))
 
-            // create Event Listener for clicking each backdrop
-            movieElement.addEventListener('click', () => this.toggleMovieView(movieElement))
+        // Create HTML Template with poster images
+        const movieTemplate = `${this.movieImages(imageURLSize)}`;
 
-            // Create HTML Template with backdrop images
-            movieTemplate = `${this.movieBackdrop(imageURLSize)}`;
-
-            // Delete moviePosters div if there is no posterpath in this object
-            if (this.movieBackdrop() == null) {
-                movieElement.parentNode.removeChild(movieElement)
-            }
-
-            // Add template to div HTML & append
-            movieElement.innerHTML = movieTemplate
-            appendDestination.appendChild(movieElement)
-
-        } else {
-            // Create Div & change class
-            movieElement = document.createElement('div')
-            movieElement.className = 'moviePosters'
-
-            // create Event Listener for clicking each poster
-            movieElement.addEventListener('click', () => this.toggleMovieView(movieElement))
-
-            // Create HTML Template with poster images
-            movieTemplate = `${this.movieImages(imageURLSize)}`;
-
-            // Delete moviePoster div if there is no posterpath in this object
-            if (this.movieImages() == null) {
-                movieElement.parentNode.removeChild(movieElement)
-            }
-
-            // Add template to div HTML & append
-            movieElement.innerHTML = movieTemplate
-            appendDestination.appendChild(movieElement)
+        // Delete moviePoster div if there is no posterpath in this object
+        if (this.movieImages() == null) {
+            movieElement.parentNode.removeChild(movieElement)
         }
 
+        // Add template to div HTML & append
+        movieElement.innerHTML = movieTemplate
+        appendDestination.appendChild(movieElement)
     }
     
     movieImages(imageURLSize) {
@@ -503,15 +315,15 @@ class movie {
     }
 
     createMovieSlide(imageURLSize) {
+        // create slider image div (slide) => create template add to innerhtml 
         let slide = document.createElement('div')
         slide.className = 'backdropimage'
 
         let slideTemplate = `${this.movieBackdrop(imageURLSize)}`
+        slide.innerHTML = slideTemplate
 
         // create Event Listener for clicking each backdrop
         slide.addEventListener('click', () => this.toggleMovieView())
-
-        slide.innerHTML = slideTemplate
 
         return slide 
     }
@@ -521,14 +333,28 @@ class movie {
         if (this.backdropPath == null) {
             return null
         } else {
-            return `
-                        <img src=${imageURLSize + this.backdropPath} data-movie-id=${this.id}/>
-                        <div class='backdropinfo'>
-                            <p>Trending Movies</p>
-                            <h1>${this.title}</h1>
-                        </div>
-                    `;  
+            return `<img src=${imageURLSize + this.backdropPath} data-movie-id=${this.id}/>`;  
         }
+    }
+
+    buildSliderTitle() {
+        // Create Div & change class
+        const sliderTitle = document.createElement('div')
+        sliderTitle.className = 'sliderTitle'
+
+        // Create html Template => add to div innerHTML 
+        let sliderTitleTemplateHTML = `${this.sliderTitleTemplate()}`
+        sliderTitle.innerHTML = sliderTitleTemplateHTML
+
+        return sliderTitle
+    }
+
+    sliderTitleTemplate() {
+        return `    <div class='backdropinfo'>
+                        <p>Trending Movies</p>
+                        <h1>${this.title}</h1>
+                    </div>`
+        
     }
 
     toggleMovieView() {
@@ -541,7 +367,7 @@ class movie {
         viewModeDiv.innerHTML = viewModeTemplate
         document.body.appendChild(viewModeDiv)
 
-        // viewModeDiv.addEventListener('click', () => removeDiv('.viewmode'))
+        // Adds eventlistener to exitbutton which triggers and removes the viewmode from DOM
         document.querySelector('.viewmode__exitbutton').addEventListener('click', () => removeDiv('.viewmode'))
     }
 
